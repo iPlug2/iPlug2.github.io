8c00f02d59e93be452e701dd4e0cc1f8cb05165c
Oli Larkin <olilarkin@googlemail.com>
2019-01-27 23:57:16 +0000

    oops

---
+++ canvas/IGraphicsStressTest/scripts/IGraphicsStressTest-awp.js
@@ -0,0 +1,10 @@
+class IGraphicsStressTest_AWP extends AudioWorkletGlobalScope.WAMProcessor
+{
+  constructor(options) {
+    options = options || {}
+    options.mod = AudioWorkletGlobalScope.WAM.IGraphicsStressTest;
+    super(options);
+  }
+}
+
+registerProcessor("IGraphicsStressTest", IGraphicsStressTest_AWP);
