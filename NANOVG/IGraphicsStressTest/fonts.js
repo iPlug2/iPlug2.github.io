552a32482b49969df2cb7d2a68d1e7fdb4fdd580
olilarkin <olilarkin@googlemail.com>
2019-07-16 17:11:35 +0000

    publish via pipeline

--- NANOVG/IGraphicsStressTest/fonts.js
+++ NANOVG/IGraphicsStressTest/fonts.js
@@ -175,6 +175,6 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 145348, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 145348, "package_uuid": "76fa0d17-3891-4722-8ae2-a7fadb13f252"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 145348, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 145348, "package_uuid": "1695e5ac-24ae-4485-83f2-36cb9c5ee2ae"});
 
 })();
--- NANOVG/IGraphicsStressTest/svgs.js
+++ NANOVG/IGraphicsStressTest/svgs.js
@@ -175,6 +175,6 @@ Module['FS_createPath']('/resources', 'img', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 96719, "filename": "/resources/img/23.svg"}], "remote_package_size": 96719, "package_uuid": "9642c39b-1bcd-4fbd-a0c4-6af2c907b2fd"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 96719, "filename": "/resources/img/23.svg"}], "remote_package_size": 96719, "package_uuid": "3818d33c-6137-4a4f-a9e4-e47c851a21f1"});
 
 })();
