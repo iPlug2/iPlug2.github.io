b7bb9c67445f4ea69fd035a75f955e5f2f60f37c
olilarkin <olilarkin@googlemaill.com>
2020-02-10 21:09:38 +0000

    publish via pipeline

--- CANVAS/IPlugControls/svgs.js
+++ CANVAS/IPlugControls/svgs.js
@@ -175,7 +175,7 @@ Module['FS_createPath']('/resources', 'img', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 2477, "filename": "/resources/img/vector-knob.svg"}], "remote_package_size": 2477, "package_uuid": "e1abdde9-f4a4-4240-b718-4f5db07c3083"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 2477, "filename": "/resources/img/vector-knob.svg"}], "remote_package_size": 2477, "package_uuid": "5722acdb-40c0-433c-8a11-0e91750dbffa"});
   
   })();
   
