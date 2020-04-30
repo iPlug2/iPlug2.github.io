6bc0bf1c4e4d75d36a5e9928508828f6647433dd
olilarkin <olilarkin@googlemaill.com>
2020-04-30 19:41:12 +0000

    publish via pipeline

--- NANOVG/IPlugControls/scripts/IPlugControls-awn.js
+++ NANOVG/IPlugControls/scripts/IPlugControls-awn.js
@@ -78,5 +78,9 @@ class IPlugControlsController extends WAMController
       Module.SSMFD(parseInt(msg.prop), data.length, buffer);
       Module._free(buffer);
     }
+    else if(msg.verb == "StartIdleTimer") {
+      Module.StartIdleTimer();
+    }
+
   }
 }
--- NANOVG/IPlugControls/svgs.js
+++ NANOVG/IPlugControls/svgs.js
@@ -176,7 +176,7 @@ Module['FS_createPath']('/resources', 'img', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 1226, "filename": "/resources/img/slider-track.svg"}, {"start": 1226, "audio": 0, "end": 3703, "filename": "/resources/img/vector-knob.svg"}, {"start": 3703, "audio": 0, "end": 4901, "filename": "/resources/img/slider-handle.svg"}], "remote_package_size": 4901, "package_uuid": "bef887e8-eb70-427f-9389-7c2a0e582e0a"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 1226, "filename": "/resources/img/slider-track.svg"}, {"start": 1226, "audio": 0, "end": 3703, "filename": "/resources/img/vector-knob.svg"}, {"start": 3703, "audio": 0, "end": 4901, "filename": "/resources/img/slider-handle.svg"}], "remote_package_size": 4901, "package_uuid": "ddb00752-b445-47e2-b5e0-9f6bb35c308e"});
   
   })();
   
