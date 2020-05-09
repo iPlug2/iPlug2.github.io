fd30d4fa02c0a4f3cffdfda882d6ab5f57ad0f01
olilarkin <olilarkin@googlemaill.com>
2020-05-09 14:14:31 +0000

    publish via pipeline

--- NANOVG/IPlugMidiEffect/fonts.js
+++ NANOVG/IPlugMidiEffect/fonts.js
@@ -176,7 +176,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 145348, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 145348, "package_uuid": "b5585965-040c-4e32-95e7-1cd2ec2d3046"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 145348, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 145348, "package_uuid": "25d76917-6a20-45db-9d0d-29c3dd4e65c7"});
   
   })();
   
