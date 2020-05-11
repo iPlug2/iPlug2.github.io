e31dfaa26208f156ca8c492237fca250969eb352
olilarkin <olilarkin@googlemaill.com>
2020-05-11 15:31:03 +0000

    publish via pipeline

--- NANOVG/IGraphicsTest/svgs.js
+++ NANOVG/IGraphicsTest/svgs.js
@@ -176,7 +176,7 @@ Module['FS_createPath']('/resources', 'img', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 96719, "filename": "/resources/img/23.svg"}, {"start": 96719, "audio": 0, "end": 99837, "filename": "/resources/img/orbs.svg"}], "remote_package_size": 99837, "package_uuid": "de446b92-a002-49e9-8574-94f4e15318b3"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 96719, "filename": "/resources/img/23.svg"}, {"start": 96719, "audio": 0, "end": 99837, "filename": "/resources/img/orbs.svg"}], "remote_package_size": 99837, "package_uuid": "61491b14-22ab-4567-b832-5e6724ca4ffd"});
   
   })();
   
