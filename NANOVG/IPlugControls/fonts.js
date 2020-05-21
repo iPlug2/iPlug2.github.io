03353aaf7e4d395df4169f40c549fa6f992f4d89
olilarkin <olilarkin@googlemaill.com>
2020-05-21 11:12:31 +0000

    publish via pipeline

--- NANOVG/IPlugControls/fonts.js
+++ NANOVG/IPlugControls/fonts.js
@@ -176,7 +176,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 36948, "filename": "/resources/fonts/fontaudio.ttf"}, {"start": 36948, "audio": 0, "end": 225704, "filename": "/resources/fonts/forkawesome-webfont.ttf"}, {"start": 225704, "audio": 0, "end": 371052, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 371052, "package_uuid": "6cebdb47-fd61-496f-994d-439c4284ff63"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 36948, "filename": "/resources/fonts/fontaudio.ttf"}, {"start": 36948, "audio": 0, "end": 225704, "filename": "/resources/fonts/forkawesome-webfont.ttf"}, {"start": 225704, "audio": 0, "end": 371052, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 371052, "package_uuid": "03498c66-6871-4631-883f-75d8ecdea7b7"});
   
   })();
   
