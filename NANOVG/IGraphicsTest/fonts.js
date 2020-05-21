e975cd498c165b899a76308ecdd9c5244dd1bad9
olilarkin <olilarkin@googlemaill.com>
2020-05-21 11:12:55 +0000

    publish via pipeline

--- NANOVG/IGraphicsTest/fonts.js
+++ NANOVG/IGraphicsTest/fonts.js
@@ -176,7 +176,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 263712, "filename": "/resources/fonts/Montserrat-LightItalic.ttf"}, {"start": 263712, "audio": 0, "end": 409060, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 409060, "package_uuid": "c7bf2f3e-d97c-4d44-a948-95f4022a8dc2"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 263712, "filename": "/resources/fonts/Montserrat-LightItalic.ttf"}, {"start": 263712, "audio": 0, "end": 409060, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 409060, "package_uuid": "a5557bc5-4d9c-4f19-a7a0-4395ffae05ff"});
   
   })();
   
