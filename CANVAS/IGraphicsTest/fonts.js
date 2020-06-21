b7b5c44bf694f83bc8a3e480b7e23311b0947699
olilarkin <olilarkin@googlemaill.com>
2020-06-21 22:51:31 +0000

    publish via pipeline

--- CANVAS/IGraphicsTest/fonts.js
+++ CANVAS/IGraphicsTest/fonts.js
@@ -174,7 +174,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 263712, "filename": "/resources/fonts/Montserrat-LightItalic.ttf"}, {"start": 263712, "audio": 0, "end": 409060, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 409060, "package_uuid": "197321b1-2aea-4c6c-8218-a308f6ed6f7d"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 263712, "filename": "/resources/fonts/Montserrat-LightItalic.ttf"}, {"start": 263712, "audio": 0, "end": 409060, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 409060, "package_uuid": "9ccac034-568e-4fc6-ba04-fe89c9fcc2fa"});
   
   })();
   
