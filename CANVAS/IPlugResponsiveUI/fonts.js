d6baa77c3948ecc57c39a1ee7bdf3c2bde451896
olilarkin <olilarkin@googlemaill.com>
2020-08-01 09:15:01 +0000

    publish via pipeline

--- CANVAS/IPlugResponsiveUI/fonts.js
+++ CANVAS/IPlugResponsiveUI/fonts.js
@@ -174,7 +174,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"start": 0, "audio": 0, "end": 145348, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 145348, "package_uuid": "e8370111-d1bd-45d9-a55c-dd462bd1a402"});
+   loadPackage({"files": [{"start": 0, "audio": 0, "end": 145348, "filename": "/resources/fonts/Roboto-Regular.ttf"}], "remote_package_size": 145348, "package_uuid": "275e106f-4c91-435b-b1e6-93456982d2b2"});
   
   })();
   
