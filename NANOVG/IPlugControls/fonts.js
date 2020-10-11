8edfe51c1181d9869353e524f9380b69bf6d77b6
olilarkin <olilarkin@googlemaill.com>
2020-10-11 22:44:30 +0000

    publish via pipeline

--- NANOVG/IPlugControls/fonts.js
+++ NANOVG/IPlugControls/fonts.js
@@ -171,7 +171,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}, {"filename": "/resources/fonts/fontaudio.ttf", "start": 145348, "end": 182296, "audio": 0}, {"filename": "/resources/fonts/forkawesome-webfont.ttf", "start": 182296, "end": 371052, "audio": 0}], "remote_package_size": 371052, "package_uuid": "318ef572-65ba-4dd5-8773-7800a09c70e7"});
+   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}, {"filename": "/resources/fonts/fontaudio.ttf", "start": 145348, "end": 182296, "audio": 0}, {"filename": "/resources/fonts/forkawesome-webfont.ttf", "start": 182296, "end": 371052, "audio": 0}], "remote_package_size": 371052, "package_uuid": "b9e0b822-1a4d-46b4-b907-9330ab28646d"});
   
   })();
   
--- NANOVG/IPlugControls/imgs@2x.js
+++ NANOVG/IPlugControls/imgs@2x.js
@@ -350,7 +350,7 @@ Module['FS_createPath']('/resources', 'img', true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/img/switch@2x.png", "start": 0, "end": 8053, "audio": 0}, {"filename": "/resources/img/font@2x.png", "start": 8053, "end": 29257, "audio": 0}, {"filename": "/resources/img/knob-rotate@2x.png", "start": 29257, "end": 44879, "audio": 0}, {"filename": "/resources/img/slider-track@2x.png", "start": 44879, "end": 45999, "audio": 0}, {"filename": "/resources/img/button@2x.png", "start": 45999, "end": 155210, "audio": 0}, {"filename": "/resources/img/slider-handle@2x.png", "start": 155210, "end": 163546, "audio": 0}, {"filename": "/resources/img/knob@2x.png", "start": 163546, "end": 632738, "audio": 0}], "remote_package_size": 632738, "package_uuid": "7f58fe26-02aa-4379-81ad-7f1419bbb3bf"});
+   loadPackage({"files": [{"filename": "/resources/img/switch@2x.png", "start": 0, "end": 8053, "audio": 0}, {"filename": "/resources/img/font@2x.png", "start": 8053, "end": 29257, "audio": 0}, {"filename": "/resources/img/knob-rotate@2x.png", "start": 29257, "end": 44879, "audio": 0}, {"filename": "/resources/img/slider-track@2x.png", "start": 44879, "end": 45999, "audio": 0}, {"filename": "/resources/img/button@2x.png", "start": 45999, "end": 155210, "audio": 0}, {"filename": "/resources/img/slider-handle@2x.png", "start": 155210, "end": 163546, "audio": 0}, {"filename": "/resources/img/knob@2x.png", "start": 163546, "end": 632738, "audio": 0}], "remote_package_size": 632738, "package_uuid": "cdf02851-2f11-41cf-8492-b3eeffbebfdb"});
   
   })();
   
