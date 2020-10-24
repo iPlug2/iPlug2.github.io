341b1b6f2e356cf4dd978dcc1b3d6fc65b53f156
olilarkin <olilarkin@googlemaill.com>
2020-10-24 15:47:39 +0000

    publish via pipeline

--- NANOVG/IPlugControls/imgs.js
+++ NANOVG/IPlugControls/imgs.js
@@ -350,7 +350,7 @@ Module['FS_createPath']('/resources', 'img', true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/img/button.png", "start": 0, "end": 30632, "audio": 0}, {"filename": "/resources/img/slider-handle.png", "start": 30632, "end": 34051, "audio": 0}, {"filename": "/resources/img/knob.png", "start": 34051, "end": 103537, "audio": 0}, {"filename": "/resources/img/switch.png", "start": 103537, "end": 106729, "audio": 0}, {"filename": "/resources/img/font.png", "start": 106729, "end": 115475, "audio": 0}, {"filename": "/resources/img/slider-track.png", "start": 115475, "end": 118005, "audio": 0}, {"filename": "/resources/img/knob-rotate.png", "start": 118005, "end": 124542, "audio": 0}], "remote_package_size": 124542, "package_uuid": "b78e9bc7-1bc8-4436-99d6-4e30b699c630"});
+   loadPackage({"files": [{"filename": "/resources/img/button.png", "start": 0, "end": 30632, "audio": 0}, {"filename": "/resources/img/slider-handle.png", "start": 30632, "end": 34051, "audio": 0}, {"filename": "/resources/img/knob.png", "start": 34051, "end": 103537, "audio": 0}, {"filename": "/resources/img/switch.png", "start": 103537, "end": 106729, "audio": 0}, {"filename": "/resources/img/font.png", "start": 106729, "end": 115475, "audio": 0}, {"filename": "/resources/img/slider-track.png", "start": 115475, "end": 118005, "audio": 0}, {"filename": "/resources/img/knob-rotate.png", "start": 118005, "end": 124542, "audio": 0}], "remote_package_size": 124542, "package_uuid": "db8eb952-54dd-41c0-9b1f-a2e723f9d66e"});
   
   })();
   
