06d4417743dd4fc501656e963e284b4d653f8621
olilarkin <olilarkin@googlemaill.com>
2020-11-02 15:10:03 +0000

    publish via pipeline

--- CANVAS/IGraphicsTest/svgs.js
+++ CANVAS/IGraphicsTest/svgs.js
@@ -171,7 +171,7 @@ Module['FS_createPath']('/resources', 'img', true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/img/orbs.svg", "start": 0, "end": 3118, "audio": 0}, {"filename": "/resources/img/23.svg", "start": 3118, "end": 99837, "audio": 0}], "remote_package_size": 99837, "package_uuid": "2ef33904-e79f-49b3-b41e-e8edf127af64"});
+   loadPackage({"files": [{"filename": "/resources/img/orbs.svg", "start": 0, "end": 3118, "audio": 0}, {"filename": "/resources/img/23.svg", "start": 3118, "end": 99837, "audio": 0}], "remote_package_size": 99837, "package_uuid": "7b39f041-2ab7-4fb1-81e2-4d5170dd8c5d"});
   
   })();
   
